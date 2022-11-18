04d9e93719b746e3ba32e890de24197e3ce0418a
kravninja <tita0@chicagobooth.edu>
2022-11-18 14:29:42 +0000

    Added has many

--- app/models/director.rb
+++ app/models/director.rb
@@ -11,11 +11,13 @@
 #  updated_at :datetime         not null
 #
 class Director < ApplicationRecord
-  def filmography
-    key = self.id
+  has_many(:filmography, {:class_name =>"Movie", :foreign_key =>"director_id" })
 
-    the_many = Movie.where({ :director_id => key })
+  # def filmography
+  #   key = self.id
 
-    return the_many
-  end
+  #   the_many = Movie.where({ :director_id => key })
+
+  #   return the_many
+  # end
 end
